adb58642d74f407dc79d812b9c1bcfc22a818f17
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-30 07:55:15 -0600

    Set up first test suite for Bands
    First test: should get name of the band
    
    Fails initially. Needs Band model

--- models.test.js
+++ models.test.js
@@ -4,3 +4,16 @@ const chai = require('chai');
 const { expect } = require('chai');
 const sinon = require('sinon');
 
+const Band = require('./models');
+
+describe('Bands', () => { // Bands test suite
+  describe('getBandName', () => {
+    it('should get the band\'s name', () => {
+      const band = new Band({
+        name: 'Cool Band Name',
+        genre: 'Awesome genre',
+      });
+      expect(band.getBandName()).to.equal('Cool Band Name');
+    });
+  });
+});

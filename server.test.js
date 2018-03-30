d08cbd06a5929aa060b7f3462b730a6a7a8fe5db
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-30 16:09:27 -0600

    Write assertion for POST request
    Assertion to create a new band with status of 200 and name to come from newBand object
    
    Test's assertion fails. Need to write actual post route in server.js

--- server.test.js
+++ server.test.js
@@ -2,7 +2,29 @@ const mongoose = require('mongoose');
 
 const chai = require('chai');
 const chaihttp = require('chai-http');
-const { expect } = require('expect');
+const { expect } = require('chai');
 const sinon = require('sinon');
 
 const server = require('./server');
+chai.use(chaihttp);
+
+const Band = require('./models');
+
+describe('Server', () => {
+  describe('[POST] /band', () => {
+    it('should create a new band', () => {
+      const newBand = {
+        name: 'Cool Band Name',
+        genre: 'Awesome genre',
+      };
+      chai.request(server)
+        .post('/band')
+        .send(newBand)
+        .end((err, res) => {
+          if (err) console.error(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('Cool Band Name');
+        });
+    });
+  });
+});

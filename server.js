3fbab25777a84a65eaafc23cf3bcb4e99ba78ad1
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-30 16:13:05 -0600

    Write server route for POST
    Test for POST successfully passing

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const server = express();
 server.use(express.json());
 server.use(morgan('combined'));
 
+server.post('/band', (req, res) => {
+  res.send(req.body);
+});
+
 module.exports = server;
